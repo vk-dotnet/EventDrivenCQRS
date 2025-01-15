d6a533ee025884b9df4e1c64467e6c97167d3566
volkan kocak <161289005+devvolkankocak@users.noreply.github.com>
2025-01-15 11:36:35 +0300

    fix issue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
